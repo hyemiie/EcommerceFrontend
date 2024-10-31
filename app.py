b23b7552c2d868416f42801c20922b773645cd63
hyemiie <yemiojedapo1@gmail.com>
2024-10-31 11:41:52 +0100

    update pywin

--- app.py
+++ app.py
@@ -509,7 +509,7 @@ if __name__ == "__main__":
     
     # Get port from environment variable or use default
     port = int(os.environ.get('PORT', 10000))
-    
+
     # Run the app
     app.run(host='0.0.0.0', port=port, debug=True)    
         # Populate Products table
